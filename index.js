ec6ccd4c4649d5ab6922152541c8620865f4b2d9
Jacqueline Nixon <jacquelinenixon@me.com>
2020-12-29 04:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function add() {
 
-}
+} // adds two numbers
 
 function subtract () {
 
-}
+} // subtracts two numbers
 
 function multiply () {
 
-}
+} // multiplies two numbers
 
 function divide () {
   
+} // divides one number by another
+
+function increment(n) {
+  return n++
+}
+
+function decrement(n) {
+  return n--
 }
